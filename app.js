a61bec285f5ed866971f1bc89b409fa02bd63150
Daivik Shetty <shettydaivik100@gmail.com>
2022-04-11 13:04:36 +0530

    Added CSS , Removed time

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express=require("express");
 const bodyParser=require("body-parser");
 
 const app=express();
+
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));        //for loading css along with js and ejs in localhost
 
-var items=[],times=[];
+var items=["Eat","Sleep","Code","Repeat"];
+var times=["12:00 AM","12:00 AM","12:00 AM","12:00 AM"];
 
 app.get("/",function(req,res){
 
       var today=new Date();         //object of type date is created
-      var today2=new Date();
+      // var today2=new Date();
 
 
       var options={
@@ -19,27 +22,28 @@ app.get("/",function(req,res){
             month:"long"
       };
 
-      var options2={
-            hour:"numeric",
-            minute:"numeric",
-      };
+      // var options2={
+      //       hour:"numeric",
+      //       minute:"numeric"
+      //       // second:"numeric"     //for checking if the time is working
+      // };
 
       var day=today.toLocaleDateString("en-US",options);
-      var time=today2.toLocaleTimeString("en-US",options2);
+      // var time=today2.toLocaleTimeString("en-US",options2);
 
-      times.push(time);
+      // times.push(time);
 
       res.render("index",{
             kindOfDay:day,
             newItems:items,
-            currTimes:times
+            // currTimes:times
       });
 })
 
 app.post("/",function(req,res){
       item=req.body.newItem;
       items.push(item);
-      times.push()
+      // times.push()
       res.redirect("/");
 })
 
